a1905d944ac9d99c1d67b75df72eb8131d4235db
dev7hka <dev7hka@gmail.com>
2020-09-20 13:59:31 +0300

    Continuing first day !!!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random, time
+
 """
 19.09.2020 - Hangman Project - Day 1
 
@@ -37,38 +38,40 @@ Tasks(functions):
     - man printer
     
 """
+
 class HangingMan():
-    __wordsDatabase : list = ["BATMAN", "SUPERMAN", "SPIDERMAN", "IRONMAN"]
-    __word : list = None
-    __guessWord : list = None
-    __guessLeft : int = None
-    __lettersAvailable = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    __wordsDatabase: list = ["BATMAN", "SUPERMAN", "SPIDERMAN", "IRONMAN","ITISALOOONGWORDYOUKNOW"]
+    __word: list = None
+    __guessWord: list = None
+    __guessLeft: int = None
+    __lettersAvailable = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+                          "T", "U", "V", "W", "X", "Y", "Z"]
     __wholeMan = [
-    "|----------|",
-    "|          |",
-    "|         ---",
-    "|        |   |",
-    "|         ---",
-    "|          |",
-    "|          |",
-    "|       -------",
-    "|      |   |   |",
-    "|      |   |   |",
-    "|      |   |   |",
-    "|          |",
-    "|          |",
-    "|         / \\",
-    "|        /   \\",
-    "|       /     \\",
-    "|",
-    "|",
-    "|"]
-    __hangingManCounter : int = None
+        "|----------|",
+        "|          |",
+        "|         ---",
+        "|        |   |",
+        "|         ---",
+        "|          |",
+        "|          |",
+        "|       -------",
+        "|      |   |   |",
+        "|      |   |   |",
+        "|      |   |   |",
+        "|          |",
+        "|          |",
+        "|         / \\",
+        "|        /   \\",
+        "|       /     \\",
+        "|",
+        "|",
+        "|"]
+    __hangingManCounter: int = None
 
     def __init__(self):
-        index = random.randint(0, len(self.__wordsDatabase)-1)
+        index = random.randint(0, len(self.__wordsDatabase) - 1)
         self.__word = [x for x in self.__wordsDatabase[index]]
-        self.__guessLeft = len(self.__word)-1
+        self.__guessLeft = 10
         self.__guessWord = ["_" for i in range(len(self.__word))]
         self.__hangingManCounter = 0
 
@@ -78,8 +81,8 @@ class HangingMan():
         self.letterViewer()
         print("Your Guess:", end="")
         guess = input()
-        index = self.__lettersAvailable.index(guess)
-        if index < 0 or index > len(self.__lettersAvailable):
+        index = -1 if self.__lettersAvailable.count(guess) == 0 else self.__lettersAvailable.index(guess)
+        if index < 0 or index >= len(self.__lettersAvailable):
             return -1
         else:
             letter = self.__lettersAvailable.pop(index)
@@ -88,7 +91,7 @@ class HangingMan():
     def letterChecker(self, letter):
 
         self.__guessLeft -= 1
-        if self.__word.count(letter) == 0:
+        if letter == -1 or self.__word.count(letter) == 0:
             self.__hangingManCounter += 1
             return -1
         for i in range(len(self.__word)):
@@ -96,28 +99,34 @@ class HangingMan():
                 self.__guessWord[i] = letter
         return self.__word.count(letter)
 
-    def pointCalculator(self):
-        pass
+    def pointViewer(self):
+
+        print("Point:",len(self.__word) - self.__guessWord.count("_"))
 
     def wordViewer(self):
 
-        print("".join(self.__guessWord), end="\n")
+        print("Your Guess: ", "".join(self.__guessWord), end="\n")
 
     def letterViewer(self):
 
-        print("-".join(self.__lettersAvailable), end="\n")
-
+        print("Letters: ", "-".join(self.__lettersAvailable), end="\n")
 
     def manPrinter(self):
 
-        end = self.__hangingManCounter * ( 18 // len(self.__word))
+        end = self.__hangingManCounter * int(18 / len(self.__word))
+        if len(self.__word) > 18:
+            end = self.__hangingManCounter
         if self.__guessLeft == 0 or end >= len(self.__word):
             print("\n".join(self.__wholeMan), end="\n\n")
+            self.pointViewer()
             exit()
         print("\n".join(self.__wholeMan[0:end]), end="\n\n")
-
+        self.pointViewer()
+        if self.__word == self.__guessWord:
+            print("Congratulations !!!")
+            exit()
 hangingManGame = HangingMan()
 while True:
     letter = hangingManGame.letterPicker()
-    point = hangingManGame.letterChecker(letter)
+    hangingManGame.letterChecker(letter)
     hangingManGame.manPrinter()
